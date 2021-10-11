8459dc1cfdd264f93304695542310754802534d9
Hayden Anderson <anderson.hayden@gmail.com>
2021-10-11 15:32:35 -0400

    user controller added

---
+++ controllers/UserController.js
@@ -0,0 +1,10 @@
+const { User } = require('../models')
+
+const CreateUser = async (req, res) => {
+  try {
+    const user = await User.create({ ...req.body })
+    res.send(user)
+  } catch (error) {
+    res.status(500).send({ error: error })
+  }
+}
