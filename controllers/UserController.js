dd4bcbc6f43266653f1fa8d915df32f7400e4dbc
Hayden Anderson <anderson.hayden@gmail.com>
2021-10-11 16:33:48 -0400

    User CRUD working

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -18,4 +18,35 @@ const GetUsers = async (req, res) => {
   }
 }
 
-module.exports = { CreateUser, GetUsers }
+const GetUserById = async (req, res) => {
+  try {
+    const id = req.params.user_id
+    const user = await User.findByPk(id)
+    res.send(user)
+  } catch (error) {
+    res.status(500).send({ error: error })
+  }
+}
+
+const UpdateUser = async (req, res) => {
+  try {
+    const user = await User.update(
+      { ...req.body },
+      { where: { id: req.params.user_id }, returning: true }
+    )
+    res.send(user)
+  } catch (error) {
+    res.status(500).send({ error: error })
+  }
+}
+
+const DeleteUser = async (req, res) => {
+  try {
+    await User.destroy({ where: { id: req.params.user_id } })
+    res.send({ msg: 'User deleted', payload: req.params.user_id, status: 'Ok' })
+  } catch (error) {
+    res.status(500).send({ error: error })
+  }
+}
+
+module.exports = { CreateUser, GetUsers, GetUserById, UpdateUser, DeleteUser }
--- routes/UserRouter.js
+++ routes/UserRouter.js
@@ -3,5 +3,8 @@ const controller = require('../controllers/UserController')
 
 router.post('/', controller.CreateUser)
 router.get('/', controller.GetUsers)
+router.get('/:user_id', controller.GetUserById)
+router.put('/:user_id', controller.UpdateUser)
+router.delete('/:user_id', controller.DeleteUser)
 
 module.exports = router
