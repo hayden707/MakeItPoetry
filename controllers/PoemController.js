dbf3dbca0d7a76053b4f506470fe1d46c359cd87
Hayden Anderson <anderson.hayden@gmail.com>
2021-10-12 12:03:30 -0400

    vue installed

--- controllers/PoemController.js
+++ controllers/PoemController.js
@@ -18,4 +18,44 @@ const GetPoems = async (req, res) => {
   }
 }
 
-module.exports = { CreatePoem, GetPoems }
+const GetPoemsByUser = async (req, res) => {
+  try {
+    const id = req.params.user_id
+    const poem = await NewPoem.findAll({
+      include: [{ model: User }],
+      where: { user_id: id }
+    })
+    res.send(poem)
+  } catch (error) {
+    res.status(500).send({ error: error })
+  }
+}
+
+const UpdatePoem = async (req, res) => {
+  try {
+    const poem = await NewPoem.update(
+      { ...req.body },
+      { where: { id: req.params.poem_id }, returning: true }
+    )
+    res.send(poem)
+  } catch (error) {
+    res.status(500).send({ error: error })
+  }
+}
+
+const DeletePoem = async (req, res) => {
+  try {
+    await NewPoem.destroy({ where: { id: req.params.poem_id } })
+    res.send({ msg: 'Poem deleted', payload: req.params.poem_id, status: 'Ok' })
+  } catch (error) {
+    res.status(500).send({ error: error })
+  }
+}
+
+module.exports = {
+  CreatePoem,
+  GetPoems,
+  GetPoemsByUser,
+  UpdatePoem,
+  DeletePoem
+}
--- routes/PoemRouter.js
+++ routes/PoemRouter.js
@@ -3,5 +3,8 @@ const controller = require('../controllers/PoemController')
 
 router.post('/', controller.CreatePoem)
 router.get('/', controller.GetPoems)
+router.get('/user/:user_id', controller.GetPoemsByUser)
+router.put('/:poem_id', controller.UpdatePoem)
+router.delete('/:poem_id', controller.DeletePoem)
 
 module.exports = router
