53f00f3983906786ae60202ff4745abd8bfe81b2
Hayden Anderson <anderson.hayden@gmail.com>
2021-10-12 12:09:24 -0400

    home page created

---
+++ client/src/components/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="home">
+  <h3>"(: Hello! Welcome to the Home Page :)"</h3>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'home',
+    components: {}
+  }
+</script>
