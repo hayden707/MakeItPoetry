6d89357aee9323132086b0ca2258373d7c39c185
Hayden Anderson <anderson.hayden@gmail.com>
2021-10-15 18:34:28 -0400

    removed host in config

--- config/config.js
+++ config/config.js
@@ -2,17 +2,17 @@ require('dotenv').config()
 module.exports = {
   development: {
     database: 'makepoetry_development',
-    host: '127.0.0.1',
+    // host: '127.0.0.1',
     dialect: 'postgres'
   },
   test: {
     database: 'makepoetry_test',
-    host: '127.0.0.1',
+    // host: '127.0.0.1',
     dialect: 'postgres'
   },
   production: {
     database: 'makepoetry_production',
-    host: '127.0.0.1',
+    // host: '127.0.0.1',
     dialect: 'postgres',
     dialectOptions: {
       ssl: {
