77f8c0297ebaf804e684ee3ff66027d647f131b0
Hayden Anderson <anderson.hayden@gmail.com>
2021-10-15 17:58:36 -0400

    build to dist to fix

--- server.js
+++ server.js
@@ -27,9 +27,9 @@ app.use('/auth', AuthRouter)
 // app.use('/posts', PostRouter)
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client/build')))
+  app.use(express.static(path.join(__dirname, 'client/dist')))
   app.get('*', (req, res) => {
-    res.sendFile(path.join(`${__dirname}/client/build/index.html`))
+    res.sendFile(path.join(`${__dirname}/client/dist/index.html`))
   })
 }
 
