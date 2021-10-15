a0da58b59a8e52d7a96a2ba932c630e7cb5d80c7
Hayden Anderson <anderson.hayden@gmail.com>
2021-10-15 18:37:39 -0400

    use env variable in config

--- config/config.js
+++ config/config.js
@@ -12,6 +12,7 @@ module.exports = {
   },
   production: {
     database: 'makepoetry_production',
+    use_env_variable: 'DATABASE_URL',
     // host: '127.0.0.1',
     dialect: 'postgres',
     dialectOptions: {
