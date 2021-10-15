dc62272b29f897422758d3913f39bc2477271686
Hayden Anderson <anderson.hayden@gmail.com>
2021-10-15 17:44:42 -0400

    udno test

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use('/auth', AuthRouter)
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'client/build')))
-  app.get('/', (req, res) => {
+  app.get('*', (req, res) => {
     res.sendFile(path.join(`${__dirname}/client/build/index.html`))
   })
 }
