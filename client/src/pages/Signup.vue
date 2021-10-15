d491c97a2899057c3fceaf96772e84995e9f9e46
Hayden Anderson <anderson.hayden@gmail.com>
2021-10-15 12:50:46 -0400

    updated signup form

--- client/src/pages/Signup.vue
+++ client/src/pages/Signup.vue
@@ -1,20 +1,20 @@
 <template>
   <div class="signout-page">
     <h3>:) signup page (:</h3>
-    <form @submit.prevent="submit">
+    <form @handleSubmit.prevent="submit">
 
       <div>
         <label for="name">
           name
         </label>
-        <input type="text" name="name" id="name" v-model="form.name">
+        <input type="text" name="name" id="name" @input.prevent="handleName" @value="name">
       </div>
 
       <div>
         <label for="email">
           email
         </label>
-        <input type="text" name="email" id="email" v-model="form.email">
+        <input type="text" name="email" id="email" @input.prevent="handleEmail" @value="email">
       </div>
       
       <div>
@@ -22,7 +22,15 @@
           Password
         </label>
 
-        <input type="password" name="password" id="password" v-model="form.password">
+        <input type="password" name="password" id="password" @input.prevent="handlePassword" @value="password">
+      </div>
+
+      <div>
+        <label for="confirm-password">
+          Confirm Password
+        </label>
+
+        <input type="confirm-password" name="confirm-password" id="confoirm-password" @input.prevent="hanldeConfirmPassword" @value="confirmPassword">
       </div>
 
       <div>
@@ -35,26 +43,31 @@
 </template>
 
 <script>
-import axios from 'axios'
 
 export default{
   name: 'signup',
   components: {},
-  data(){
-    return {
-      form: {
-        name: '',
-        email: '',
-        password: ''
-      }
-    }
-
-  },
+  data: ()=>({
+      name: '',
+      email: '',
+      password: '',
+      confirmPassword: ''
+    }),
   methods: {
-    async submit(form){
-      let res = axios.post('http://localhost:3000/auth/register', form)
-      console.log(res.data)
+    handleName(e) {
+        this.name = e.target.value
+      },
+    handleEmail(e) {
+      this.email=e.target.value
+    },
+    handlePassword(e) {
+      this.password=e.target.value
+    },
+    hanldeConfirmPassword(e){
+      this.confirmPassword = e.target.value
+
     }
   }
 }
+
 </script>
