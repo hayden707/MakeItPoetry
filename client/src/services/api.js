eac6a88797c5aa23712f0916a302a65740758b3c
Hayden Anderson <anderson.hayden@gmail.com>
2021-10-17 23:54:10 -0400

    adjusted url for heroku

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export const BASE_URL =
   process.env.NODE_ENV === 'production'
-    ? `${window.location.origin}`
+    ? `${window.location.origin}/api`
     : 'http://localhost:3000/api'
 
 const Client = axios.create({ baseURL: BASE_URL })
